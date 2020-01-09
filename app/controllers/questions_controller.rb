9b3489a6e5922b04b405164983c81ff0c2adc8e4
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-01-08 22:37:51 -0500

    Improvements on categpries and new questions form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
     before_action :set_question, only: [:show, :edit, :update, :destroy]
 
     def index
-        @questions = Question.all
+        @questions = Question.all.order(id: :desc)
     end
 
     def show
