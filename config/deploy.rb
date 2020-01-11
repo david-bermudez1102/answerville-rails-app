7ec55f3a10574db607bbfd8931b3e4c9661b4226
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-01-11 12:19:18 -0500

    Set up Puma, Nginx & Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application,     'answerville'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-
+set :default_shell, "/bin/bash -l"
 
 # Deploy to the user's home directory
 
