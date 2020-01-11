a436c0754cc5cdf345664d40051a5fb2ce985421
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-01-11 12:17:39 -0500

    Set up Puma, Nginx & Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,12 +7,19 @@ set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
+
+# Deploy to the user's home directory
+
+
+# Only keep the last 5 releases to save disk space
+set :keep_releases, 5
+
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :deploy_to,       "/home/deploy/#{fetch :application}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
